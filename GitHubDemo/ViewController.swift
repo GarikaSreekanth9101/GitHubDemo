5ef95652b51c64d36fd7d89ba73ce2d5dcf49f12
sai <coolkrishsai@gmail.com>
2021-02-23 16:10:00 +0530

    added a connections to lable

--- GitHubDemo/ViewController.swift
+++ GitHubDemo/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mylBL: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mylBL.text! = "sai"
         // Do any additional setup after loading the view.
     }
 
+    
+    
 
 }
 
